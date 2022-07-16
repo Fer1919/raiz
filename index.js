06d1a0109a5aa324536f64a937cb8fc249bde9d1
gitEstudio <ingespinosa1981@gmail.com>
2022-07-15 23:09:32 -0400

    raiz matematica a

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let a=+prompt('Ingrese INDICE, Número ubicado sobre la Raiz','0');
-let b=+prompt('Ingrese el SUBRADICAL, Numero ubicado dentro de la Raiz');
+let b=+prompt('Ingrese el SUBRADICAL, Numero ubicado dentro de la Raiz','0');
 let c=(b**(1/a))
 alert(`El Resultado de la Raiz es ${c} APP_Nando_Script`);
